50f34fb317aafb7e2402133d84fd717f4de6a33d
Drew <drewbepstein@gmail.com>
2019-08-23 14:56:40 -0400

    Updated comments, added License

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # SQS Sample
 #### Uses the AWS SDK for .NET
 
+*For demonstration purposes only*
 
 Instructions  
 
 1) `git clone git@github.com:itsdrew/SQSSample.git`
 1) Open in an IDE
 1) Replace the empty strings in Config.cs using the provided values
-1) Open the Program.cs file and run MainClass.Main()
+1) To generate sample messages, run Producer.main()
+1) To read and acknowledge messages, run Consumer.main()
 
 
 
--- SQSSample/Program.cs
+++ SQSSample/Program.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Net;
 using Amazon;
-using Amazon.Runtime.CredentialManagement;
 using Amazon.SQS;
 using Amazon.SQS.Model;
 using System.Linq;
@@ -11,7 +10,7 @@ namespace SQSSample {
 
 	class MainClass {
 
-		//Create the AmazonSQSClient using values from the Config.class
+		// Create the AmazonSQSClient using values from the Config.class
 		static AmazonSQSClient _client;
 		static AmazonSQSClient Client {
 			get {
@@ -28,30 +27,37 @@ namespace SQSSample {
 
 		public static void Main(string[] args) {
 
-			//Put 50 messages in the queue
+			// Put 50 messages in the queue
 			int messageCount = 50;
 			PushMessagesToQueue(" -- This is a message at " + DateTime.Now.ToShortTimeString() + " -- ", messageCount);
 
-			//Read 1 message from the queue
+			// Read 1 message from the queue
 			List<Message> messages = GetMessagesFromQueue(1);
 
 			Console.WriteLine("\nReceived {0} messages\n", messages.Count);
 
-			messages.ForEach(msg => { 
+			messages.ForEach(msg => {
+				// msg.Body is the payload of the record to be processed
 				Console.WriteLine("Message Id: {0}, Message body: {1}", msg.MessageId, msg.Body); 
 			});
 
-			//Delete message that was received (or don't)
+			// Delete message that was received (this is the acknowledgement that the message was processed)
 			messages.ForEach(msg => {
 				DeleteMessageFromQueue(msg.ReceiptHandle);
 			});
 
-
-			//Read the rest of the queue 10 at a time. 
-			//After each message is pulled, it will be invisible to any other consumers for a pre-set amount of time called the 'visibility timeout'.
-			//Visibility timeout is currently set to 10 seconds.
-			//If you don't delete the message within the visibility timeout, it can be dequeued again.
-
+			/*
+			 * Read the rest of the queue 10 messages at a time.
+			 * 
+			 * After each message is pulled, it will be invisible other consumers 
+			 * for a pre-set amount of time called the 'visibility timeout'. 
+			 * 
+			 * Visibility timeout is currently set to 10 seconds. 
+			 * 
+			 * If you don't delete the message within the visibility timeout, 
+			 * it will available again for processing.
+			 */
+			 		 
 			do {
 				messages.Clear();
 				messages = GetMessagesFromQueue(10);
@@ -66,7 +72,7 @@ namespace SQSSample {
 		}
 
 
-		//There is a batch send message method that can be used instead of this.
+		// There is a batch send message method that can be used instead of this.
 		public static void PushMessagesToQueue(string messageBody, int messageCount) {
 
 			for (int i = 0; i < messageCount; i++) {
@@ -83,7 +89,7 @@ namespace SQSSample {
 		}
 
 
-		//Returns a list of messages in the queue. Does not delete the messages. Max quantity per request is 10.
+		// Returns a list of messages in the queue. Does not delete the messages. Max quantity per request is 10.
 		public static List<Message> GetMessagesFromQueue(int quantity) {
 
 			ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest {
@@ -98,7 +104,7 @@ namespace SQSSample {
 
 
 
-		//Deletes a single message from queue.
+		// Deletes a single message from queue.
 		public static void DeleteMessageFromQueue(string receiptHandle) {
 
 			DeleteMessageRequest deleteMessageRequest = new DeleteMessageRequest {
